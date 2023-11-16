74efd06fb29ebe134d84f654da638c19d12c6d02
r.drigomachado <r.drigomachado@gmail.com>
2023-11-16 14:50:30 -0300

    Add specific script for hype

--- exp/bin/hype/hype_threads_original.sh
+++ exp/bin/hype/hype_threads_original.sh
@@ -10,23 +10,23 @@ backend=OpenMP
 
 
 #########################################################################################
-#Hype 1 e 2  - HW (32 threads)
+#Hype 1 e 2  - HW (40 threads)
 #########################################################################################
 
-output_file="threads_32.csv"
+output_file="hype_threads_40.csv"
 echo -n "" > "$output_file"
 
-echo "Hype 1 e 2 - 32 THREADS - 180" >> "$output_file"
+echo "Hype 1 e 2 - 40 THREADS - 180" >> "$output_file"
 echo "" >> "$output_file"
 
-export OMP_NUM_THREADS=32
+export OMP_NUM_THREADS=40
 for tamanho in 180; do
 for versao  in original.OpenMP.x multiple_files.OpenMP.x; do
     for i in 1 2 3; do
         if test "$versao" = "multiple_files.OpenMP.x"
         then 
             echo "MPI" >> "$output_file"
-            saida=$(mpirun --hostfile hostfile --rankfile 2_maquinas_receivers  --use-hwthread-cpus -np 3 ./multiple_files.OpenMP.x TTI $tamanho $tamanho $tamanho 16 12.5 12.5 12.5 0.000685 0.5)
+            saida=$(mpirun --hostfile hype_hostfile --rankfile hype_2_maquinas  --use-hwthread-cpus -np 3 ./multiple_files.OpenMP.x TTI $tamanho $tamanho $tamanho 16 12.5 12.5 12.5 0.000685 0.5)
             saida_formatada=$(replace_dot_with_comma)
         else
             echo "Naive" >> "$output_file"
@@ -40,23 +40,23 @@ done
 done
 
 #########################################################################################
-#Hype 1 e 2  - HW (16 threads)
+#Hype 1 e 2  - HW (20 threads)
 #########################################################################################
 
-output_file="threads_16.csv"
+output_file="hype_threads_20.csv"
 echo -n "" > "$output_file"
 
-echo "Hype 1 e 2 - 16 THREADS - 180" >> "$output_file"
+echo "Hype 1 e 2 - 20 THREADS - 180" >> "$output_file"
 echo "" >> "$output_file"
 
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=20
 for tamanho in 180; do
 for versao  in original.OpenMP.x multiple_files.OpenMP.x; do
     for i in 1 2 3; do
         if test "$versao" = "multiple_files.OpenMP.x"
         then 
             echo "MPI" >> "$output_file"
-            saida=$(mpirun --hostfile hostfile --rankfile 2_maquinas_receivers  --use-hwthread-cpus -np 3 ./multiple_files.OpenMP.x TTI $tamanho $tamanho $tamanho 16 12.5 12.5 12.5 0.000685 0.5)
+            saida=$(mpirun --hostfile hype_hostfile --rankfile hype_2_maquinas  --use-hwthread-cpus -np 3 ./multiple_files.OpenMP.x TTI $tamanho $tamanho $tamanho 16 12.5 12.5 12.5 0.000685 0.5)
             saida_formatada=$(replace_dot_with_comma)
         else
             echo "Naive" >> "$output_file"
