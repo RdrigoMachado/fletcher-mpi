234ae843d06a09d45e70b8dccb07682f1b2b6551
Rodrigo Machado <r.drigomachado@gmail.com>
2024-01-31 12:30:49 -0300

    a

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include<string.h>
+
 int main(int argc, char** argv) {
 
     MPI_Init(&argc, &argv);
@@ -19,15 +20,15 @@ int main(int argc, char** argv) {
 
     // Add additional arguments for spawned processes
     spawn_argv[argc+1] = NULL;  // Null-terminate the array
+    float onda[20];
+    for (int i = 0; i < 20; i++)
+    {
+        onda[i] = i;
+    }
 
-    
-    
     MPI_Comm_spawn("spawn.x", spawn_argv, 1, MPI_INFO_NULL, 0, MPI_COMM_SELF, &parentcomm, MPI_ERRCODES_IGNORE);
-    
-    spawn_argv[argc] = "2";
+    MPI_Send(onda, 20, MPI_FLOAT, 1, 101, parentcomm);
 
-    MPI_Comm_spawn("spawn.x", spawn_argv, 1, MPI_INFO_NULL, 0, MPI_COMM_SELF, &parentcomm, MPI_ERRCODES_IGNORE);
-    
     MPI_Finalize();
     return 0;
 }
--- spawn.c
+++ spawn.c
@@ -3,12 +3,27 @@
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
+    float onda[20];
+
 
     // Accessing command line arguments
     printf("Number of arguments: %d\n", argc);
     for (int i = 0; i < argc; ++i) {
         printf("Argument %d: %s\n", i, argv[i]);
     }
+    MPI_Comm parentcomm;
+    MPI_Comm_get_parent(&parentcomm);
+
+    MPI_Recv(onda, 20, MPI_FLOAT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
+
+
+    printf("onda\n");
+    for(int i = 0; i < 20; i++)
+    {
+        printf("%f ", onda[i]);
+    }
+    printf("\n");
+
 
     MPI_Finalize();
     return 0;
