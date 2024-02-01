bd2cda5c52a1aae9947b4d367d57319e96c66a75
Rodrigo Machado <r.drigomachado@gmail.com>
2024-02-01 01:13:14 -0300

    a

--- spawn/spawn/comunicacao.mpi.c
+++ spawn/spawn/comunicacao.mpi.c
@@ -6,12 +6,17 @@ int num_processo = 0;
 
 void MPI_enviar_onda(int sx, int sy, int sz, float *ondaPtr,  SlicePtr p) {
   int tamanho = sx * sy * sz;
-  
   num_processo++;
+
+  printf("antes do sprintf\n");
+
   char offset[6];
   sprintf(offset, "%d", num_processo);
   char size[100];
   sprintf(size, "%d", tamanho);
+  
+  
+  printf("depois do sprintf\n");
 
 
 
@@ -20,8 +25,12 @@ void MPI_enviar_onda(int sx, int sy, int sz, float *ondaPtr,  SlicePtr p) {
   spawn_argv[1] = strdup(offset);
   spawn_argv[2] = strdup(size);
 
+  printf("depois das copias strdup\n");
 
   MPI_Comm childcomm;
   MPI_Comm_spawn("spawn.x", spawn_argv, 1, MPI_INFO_NULL, 0, MPI_COMM_SELF, &childcomm, MPI_ERRCODES_IGNORE);
+
+  printf("depois spawn\n");
+
   MPI_Send(ondaPtr, tamanho, MPI_FLOAT, 0, 101, childcomm);
 }
