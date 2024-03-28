ea32be6894f4afb047b12d0dd1373cc833785c08
Rodrigo Machado <r.drigomachado@gmail.com>
2024-03-28 02:23:46 -0300

    a

--- spawn/spawn/spawn.c
+++ spawn/spawn/spawn.c
@@ -20,21 +20,24 @@ int main(int argc, char** argv) {
                 MPI_MODE_CREATE | MPI_MODE_WRONLY,
                 MPI_INFO_NULL, &thefile);
     
-    printf("My rank %d\n", rank);
+printf("My rank %d\n", rank);
 
     MPI_Recv(&tamanho, 1, MPI_INT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
-    printf("tamanho %d\n", tamanho);
-    
+printf("tamanho %d\n", tamanho);
+
     MPI_Recv(&num_escrita, 1, MPI_INT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
     while(num_escrita != TERMINAR)
     {   
         MPI_Recv((void *) onda, tamanho, MPI_FLOAT, 0, 102, parentcomm, MPI_STATUS_IGNORE);
         deslocamento = (num_escrita) * tamanho;
-        printf("rank %d escrevendo %d na posicao %d\n", rank, num_escrita, deslocamento);
+printf("rank %d escrevendo %d na posicao %d\n", rank, num_escrita, deslocamento);
         MPI_File_set_view(thefile, deslocamento * sizeof(float),
                         MPI_FLOAT, MPI_FLOAT, "native", MPI_INFO_NULL);
+printf("%d set view\n", rank);
         MPI_File_write(thefile, onda, tamanho, MPI_FLOAT, MPI_STATUS_IGNORE);
+printf("%d file write\n", rank);
         MPI_Recv(&num_escrita, 1, MPI_INT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
+printf("%d num escrita\n", rank);
     }
 
     free(onda);
