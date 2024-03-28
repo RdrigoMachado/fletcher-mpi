7f723b65e11a6c27433546b8ec1ec482b433b313
Rodrigo Machado <r.drigomachado@gmail.com>
2024-03-28 03:06:01 -0300

    a

--- spawn/spawn/spawn.c
+++ spawn/spawn/spawn.c
@@ -12,7 +12,8 @@ int main(int argc, char** argv) {
     MPI_Comm_get_parent(&parentcomm);
     MPI_File thefile;
     int rank;
-    int num_escrita, tamanho, deslocamento;
+    int num_escrita, tamanho;
+    MPI_Offset deslocamento;
     float *onda;
     
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -25,12 +26,13 @@ int main(int argc, char** argv) {
 
     MPI_Recv(&num_escrita, 1, MPI_INT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
 
-    
+    deslocamento = 70 * tamanho;
+printf("rank %d escrevendo %d na posicao %lld\n", rank, num_escrita, deslocamento);
     while(num_escrita != TERMINAR)
     {   
         MPI_Recv((void *) onda, tamanho, MPI_FLOAT, 0, 102, parentcomm, MPI_STATUS_IGNORE);
         deslocamento = (num_escrita) * tamanho;
-printf("rank %d escrevendo %d na posicao %d\n", rank, num_escrita, deslocamento);
+printf("rank %d escrevendo %d na posicao %lld\n", rank, num_escrita, deslocamento);
         MPI_File_set_view(thefile, deslocamento * sizeof(float),
                         MPI_FLOAT, MPI_FLOAT, "native", MPI_INFO_NULL);
         MPI_File_write(thefile, onda, tamanho, MPI_FLOAT, MPI_STATUS_IGNORE);
