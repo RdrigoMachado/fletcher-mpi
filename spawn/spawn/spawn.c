9c6f0f20593eadb7b981bcf23a2563dc6a0534e8
Rodrigo Machado <r.drigomachado@gmail.com>
2024-03-30 00:39:01 -0300

    adicionadas mensagens de erro no arquivo spawn.c

--- spawn/spawn/spawn.c
+++ spawn/spawn/spawn.c
@@ -2,60 +2,68 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-#define TERMINAR -1
+#define TERMINAR    -1
+#define MSG_CONTROLE 101
+#define MSG_ONDA     102
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
-
-
-    MPI_Comm parentcomm;
-    MPI_Comm_get_parent(&parentcomm);
-    MPI_File thefile;
-    int rank;
-    int num_escrita, tamanho;
-    MPI_Offset deslocamento;
+    
+    int rank, num_escrita, tamanho, sucesso;
     float *onda;
+    MPI_File arquivo;
+    MPI_Comm interCommParent;
+    MPI_Offset deslocamento;
     
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_File_open(MPI_COMM_WORLD, "TTI.rsf",
-                MPI_MODE_CREATE | MPI_MODE_WRONLY,
-                MPI_INFO_NULL, &thefile);
+    MPI_Comm_get_parent(&interCommParent);
+    MPI_Recv(&tamanho, 1, MPI_INT, 0, MSG_CONTROLE, interCommParent, MPI_STATUS_IGNORE);
     
-    MPI_Recv(&tamanho, 1, MPI_INT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
-    onda = malloc(sizeof(float) * tamanho); 
-
-    MPI_Recv(&num_escrita, 1, MPI_INT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
-
     deslocamento = 70 * (MPI_Offset) tamanho;
-printf("rank %d escrevendo %d na posicao %lld size %d\n", rank, num_escrita, deslocamento, sizeof(MPI_Offset));
+    onda = malloc(sizeof(float) * tamanho);
+    if(onda == NULL)
+    {
+        printf("#%d - Erro ao alocar memória. Terminando\n", rank);
+        MPI_Finalize();
+        return 0;
+    }
+    
+    
+    sucesso = MPI_File_open(MPI_COMM_WORLD, "TTI.rsf",
+                MPI_MODE_CREATE | MPI_MODE_WRONLY,
+                MPI_INFO_NULL, &arquivo);
+    if(sucesso != MPI_SUCCESS)
+        printf("#%d - ERRO ao abrir arquivo\n", rank);
+
+    
+    sucesso = MPI_Recv(&num_escrita, 1, MPI_INT, 0, MSG_CONTROLE, interCommParent, MPI_STATUS_IGNORE);
+    if(sucesso != MPI_SUCCESS)
+            printf("#%d - ERRO ao receber msg controle\n", rank);
+
     while(num_escrita != TERMINAR)
     {   
-printf("rank %d comecou laco\n", rank);
-
-        MPI_Recv((void *) onda, tamanho, MPI_FLOAT, 0, 102, parentcomm, MPI_STATUS_IGNORE);
+        MPI_Recv((void *) onda, tamanho, MPI_FLOAT, 0, MSG_ONDA, interCommParent, MPI_STATUS_IGNORE);
         deslocamento = (num_escrita) * (MPI_Offset) tamanho;
-printf("rank %d escrevendo %d na posicao %lld\n", rank, num_escrita, deslocamento);
-        MPI_File_set_view(thefile, deslocamento * sizeof(float),
-                        MPI_FLOAT, MPI_FLOAT, "native", MPI_INFO_NULL);
 
-printf("rank %d escrevendo\n", rank);
-        MPI_File_write(thefile, onda, tamanho, MPI_FLOAT, MPI_STATUS_IGNORE);
-printf("rank %d recebendo num escrita\n", rank);
-        MPI_Recv(&num_escrita, 1, MPI_INT, 0, 101, parentcomm, MPI_STATUS_IGNORE);
-printf("rank %d recebeu instrucoes\n", rank);
+        sucesso = MPI_File_set_view(arquivo, deslocamento * sizeof(float),
+                        MPI_FLOAT, MPI_FLOAT, "native", MPI_INFO_NULL);
+        if(sucesso != MPI_SUCCESS)
+            printf("#%d - ERRO ao definir view\n", rank);
+
+        sucesso = MPI_File_write(arquivo, onda, tamanho, MPI_FLOAT, MPI_STATUS_IGNORE);
+        if(sucesso != MPI_SUCCESS)
+            printf("#%d - ERRO ao escrever no arquivo\n", rank);
+
+        sucesso = MPI_Recv(&num_escrita, 1, MPI_INT, 0, MSG_CONTROLE, interCommParent, MPI_STATUS_IGNORE);
+        if(sucesso != MPI_SUCCESS)
+            printf("#%d - ERRO ao receber msg controle\n", rank);
 
     }
     
-
-printf("Barreira child %d waiting\n", rank);
-    MPI_Barrier(MPI_COMM_WORLD);
-printf("Barreira child %d ended\n", rank);
-    
-
-printf("RANK %d exiting\n", rank);
     free(onda);
-    MPI_File_close(&thefile);
+    sucesso = MPI_File_close(&arquivo);
+    if(sucesso != MPI_SUCCESS)
+            printf("#%d - ERRO ao fechar arquivo\n", rank);
     MPI_Finalize();
-printf("RANK %d returning\n", rank);
     return 0;
 }
